07c5c7f26960084a171406ebebdad37255471b50
Marcus Bergner <marcus.bergner@saxsys.de>
2020-02-27 11:10:03 +0100

    feature/6Step-by-step

---
+++ angular-testing-course-1-start/src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,12 @@
+describe("CalculatorService", () => {
+  // mit it() wird der Testfall kurz beschrieben, es sollte aussagekräftig ->funktionelle Beschreibung
+  it("should add two numbers", () => {
+    pending();
+  });
+  it("should subtract two numbers", () => {
+    // fail () -> wird in Karma als failure ausgegeben und mit einem "debug" stacktrace ausgegeben
+    // pending () -> utility-function
+    // pending();
+    fail();
+  });
+});
