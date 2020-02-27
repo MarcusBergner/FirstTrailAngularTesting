6e81c23270647b4312c70d1f8508cbcb28812b37
Marcus Bergner <marcus.bergner@saxsys.de>
2020-02-27 14:43:12 +0100

    10. Angular-Unit-Testing BestPrctices and commonly used TestUtilities

--- angular-testing-course-1-start/src/app/courses/services/calculator.service.spec.ts
+++ angular-testing-course-1-start/src/app/courses/services/calculator.service.spec.ts
@@ -3,6 +3,12 @@ import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
 
 // describe() bschreibt die zu testende Klasse oder methode, eindeutig erkennbar beschreiben!
+//--> mit ein "x" vorangestellt disable-this.Test !
+//--> mit ein "f" vorangestellt focused_on these particular Test !
+// Es sollten immer nach folgendem Shema definiert sein:
+//  1. setup the test --> beforeEach(()=> {});
+//  2. perform the operation that want to test --> console.log("add test"); const result = calculator.add(2, 2);
+//  3. run series of assertions, that will pass or fail -->  expect(loggerSpy.log).toHaveBeenCalledTimes(1);
 describe("CalculatorService", () => {
   // alle zu verwendenden variablen, müssen wie in Java auch,
   // außerhalb mit "let" inizialisiert werden!
@@ -10,6 +16,7 @@ describe("CalculatorService", () => {
   // beforeEach() ist wie in Java @BeforeClass(), um eventuelle Enviroments zu setzen!
   beforeEach(() => {
     console.log("Calling beforeEach()");
+    // createSpyObj() ==  stellt ein mock-Objekt zur Verfügung
     loggerSpy = jasmine.createSpyObj("LoggerService", ["log"]);
 
     // TestBed.configureTestingModule({}) --> kreiert ein Testmodule mit möglichen Services-Abhängigkeiten,  (Jasmine-unabhängiges-Object)
@@ -31,9 +38,9 @@ describe("CalculatorService", () => {
     calculator = TestBed.inject(CalculatorService);
   });
 
-  // mit it() wird der Testfall kurz beschrieben, es sollte aussagekräftig ->funktionelle Beschreibung
+  // mit it() wird ein(Unit) Testfall kurz beschrieben, es sollte aussagekräftig ->funktionelle Beschreibung
   // mit spyOn() -> utilityFunction von Jasmine wird überprüft das der funktionsaufruf nur einmal ausgeführt wird beim Testaufruf
-  it("should add two numbers", () => {
+  fit("should add two numbers", () => {
     // zuerst wird eine aktuelle Instanz des zu testenendes Objektes deklareiert(FakeObjekt)
     // const logger = new LoggerService();
     // spyOn() - erstes argument ist das zu prüfende objekt,
