1662ce9159ff14f8e7d63de6a187fbce9cb3c26f
Marcus Bergner <marcus.bergner@saxsys.de>
2020-02-27 13:20:22 +0100

    Strcuturing Angular Unit-Tests, Test setup using beforeEach

--- angular-testing-course-1-start/src/app/courses/services/calculator.service.spec.ts
+++ angular-testing-course-1-start/src/app/courses/services/calculator.service.spec.ts
@@ -1,32 +1,39 @@
 import { CalculatorService } from "./calculator.service";
-import { LoggerService } from "./logger.service";
 
 // describe() bschreibt die zu testende Klasse oder methode, eindeutig erkennbar beschreiben!
 describe("CalculatorService", () => {
+  // alle zu verwendenden variablen, müssen wie in Java auch,
+  // außerhalb mit "let" inizialisiert werden!
+  let calculator: CalculatorService, loggerSpy: any;
+  // beforeEach() ist wie in Java @BeforeClass(), um eventuelle Enviroments zu setzen!
+  beforeEach(() => {
+    console.log("Calling beforeEach()");
+    loggerSpy = jasmine.createSpyObj("LoggerService", ["log"]);
+    calculator = new CalculatorService(loggerSpy);
+  });
+
   // mit it() wird der Testfall kurz beschrieben, es sollte aussagekräftig ->funktionelle Beschreibung
   // mit spyOn() -> utilityFunction von Jasmine wird überprüft das der funktionsaufruf nur einmal ausgeführt wird beim Testaufruf
   it("should add two numbers", () => {
     // zuerst wird eine aktuelle Instanz des zu testenendes Objektes deklareiert(FakeObjekt)
     // const logger = new LoggerService();
-    const logger = jasmine.createSpyObj("LoggerService", ["log"]);
-
     // spyOn() - erstes argument ist das zu prüfende objekt,
     // zweites argument ist eine liste an Methoden die geprüft werden sollen.
     // spyOn(logger, "log");
-    const calculator = new CalculatorService(logger);
+    console.log("add test");
     const result = calculator.add(2, 2);
     // sorgt dafür das der Test fehlschlägt wenn das Errgebnis nicht 4 ist.
     expect(result).toBe(4);
     // toHaveBeenCalledTimes() beschreibt wie oft diese methode/ function ausgeführt werden soll!
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
   it("should subtract two numbers", () => {
-    // fail () -> wird in Karma als failure ausgegeben und mit einem "debug" stacktrace ausgegeben
+    // fail () -> wird in Karma als failure ausgegeben und mit einem "debug" stacktrace ausgegeben, auch wenn alles korrekt ist
     // pending () -> utility-function
-    // pending();
     // fail();
-    const calculator = new CalculatorService(new LoggerService());
+    console.log("subtract test");
     const result = calculator.subtract(2, 2);
     expect(result).toBe(0, "unexpected subtrraction result");
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 });
