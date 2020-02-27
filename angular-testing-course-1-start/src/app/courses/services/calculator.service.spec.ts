a9d15e2b3a5d1a50038885f9578e7df60b2d82e1
Marcus Bergner <marcus.bergner@saxsys.de>
2020-02-27 13:02:37 +0100

    7. Unittests mit Jasmine introdruction to jasmine spies

--- angular-testing-course-1-start/src/app/courses/services/calculator.service.spec.ts
+++ angular-testing-course-1-start/src/app/courses/services/calculator.service.spec.ts
@@ -1,16 +1,24 @@
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
 
+// describe() bschreibt die zu testende Klasse oder methode, eindeutig erkennbar beschreiben!
 describe("CalculatorService", () => {
   // mit it() wird der Testfall kurz beschrieben, es sollte aussagekräftig ->funktionelle Beschreibung
-  // mit spy() wird überprüft das der funktionsaufruf nur einmal ausgeführt wird beim Testaufruf
+  // mit spyOn() -> utilityFunction von Jasmine wird überprüft das der funktionsaufruf nur einmal ausgeführt wird beim Testaufruf
   it("should add two numbers", () => {
-    // pending();
-    const logger = new LoggerService();
+    // zuerst wird eine aktuelle Instanz des zu testenendes Objektes deklareiert(FakeObjekt)
+    // const logger = new LoggerService();
+    const logger = jasmine.createSpyObj("LoggerService", ["log"]);
+
+    // spyOn() - erstes argument ist das zu prüfende objekt,
+    // zweites argument ist eine liste an Methoden die geprüft werden sollen.
+    // spyOn(logger, "log");
     const calculator = new CalculatorService(logger);
     const result = calculator.add(2, 2);
     // sorgt dafür das der Test fehlschlägt wenn das Errgebnis nicht 4 ist.
     expect(result).toBe(4);
+    // toHaveBeenCalledTimes() beschreibt wie oft diese methode/ function ausgeführt werden soll!
+    expect(logger.log).toHaveBeenCalledTimes(1);
   });
   it("should subtract two numbers", () => {
     // fail () -> wird in Karma als failure ausgegeben und mit einem "debug" stacktrace ausgegeben
