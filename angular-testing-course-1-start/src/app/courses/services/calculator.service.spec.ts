3b81a7c1fb2aa68ee3adc630449c4765e42b127a
Marcus Bergner <marcus.bergner@saxsys.de>
2020-02-27 11:23:13 +0100

    6.first jasmine testCase definition

--- angular-testing-course-1-start/src/app/courses/services/calculator.service.spec.ts
+++ angular-testing-course-1-start/src/app/courses/services/calculator.service.spec.ts
@@ -1,12 +1,24 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
 describe("CalculatorService", () => {
   // mit it() wird der Testfall kurz beschrieben, es sollte aussagekräftig ->funktionelle Beschreibung
+  // mit spy() wird überprüft das der funktionsaufruf nur einmal ausgeführt wird beim Testaufruf
   it("should add two numbers", () => {
-    pending();
+    // pending();
+    const logger = new LoggerService();
+    const calculator = new CalculatorService(logger);
+    const result = calculator.add(2, 2);
+    // sorgt dafür das der Test fehlschlägt wenn das Errgebnis nicht 4 ist.
+    expect(result).toBe(4);
   });
   it("should subtract two numbers", () => {
     // fail () -> wird in Karma als failure ausgegeben und mit einem "debug" stacktrace ausgegeben
     // pending () -> utility-function
     // pending();
-    fail();
+    // fail();
+    const calculator = new CalculatorService(new LoggerService());
+    const result = calculator.subtract(2, 2);
+    expect(result).toBe(0, "unexpected subtrraction result");
   });
 });
