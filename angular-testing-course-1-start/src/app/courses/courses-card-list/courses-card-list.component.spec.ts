87976c3f407a5c380fea52d26350667650cc3f66
Marcus Bergner <marcus.bergner@saxsys.de>
2020-02-28 11:47:26 +0100

    18. Angular Component testing - Test setup with the async test Utiity

--- angular-testing-course-1-start/src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ angular-testing-course-1-start/src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,14 +1,30 @@
-import { TestBed } from "@angular/core/testing";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { CoursesModule } from "../courses.module";
+import { CoursesCardListComponent } from "./courses-card-list.component";
 
 describe("CoursesCardListComponent", () => {
-  beforeEach(() => {
+  let component: CoursesCardListComponent;
+  let fixture: ComponentFixture<CoursesCardListComponent>;
+
+  // async() --> wait for any asynchconous operations,
+  // that we triggered by the code passed with to complete in this Test-Block (default timeout 5 sek) !
+  // after that, beforeEach() runs
+  // here is the case: detect that the promise was lunched here and wait for the promise to complete
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [CoursesModule]
-    });
-  });
+      // only safe way to execute code after compilation finished,
+      // and wait for promise to resolve --> .then(() =>)
+    })
+      .compileComponents()
+      .then(() => {
+        // setup our test
+        fixture = TestBed.createComponent(CoursesCardListComponent);
+        component = fixture.componentInstance;
+      });
+  }));
   it("should create the component", () => {
-    pending();
+    expect(component).toBeTruthy();
   });
 
   it("should display the course list", () => {
