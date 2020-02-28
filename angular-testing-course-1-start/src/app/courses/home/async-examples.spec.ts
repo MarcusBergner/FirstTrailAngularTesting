5f5f3f0e96cb3596c160a764632bc507b8d5c80c
Marcus Bergner <marcus.bergner@saxsys.de>
2020-02-28 16:55:08 +0100

    28. Understanding Angular fakeAsync Testing Zone

--- angular-testing-course-1-start/src/app/courses/home/async-examples.spec.ts
+++ angular-testing-course-1-start/src/app/courses/home/async-examples.spec.ts
@@ -1,4 +1,6 @@
-fdescribe("Async Testing Examples", () => {
+import { fakeAsync, tick } from "@angular/core/testing";
+
+describe("Async Testing Examples", () => {
   it("Asynchronous test example with Jasmine done()", (done: DoneFn) => {
     let test = false;
 
@@ -9,4 +11,20 @@ fdescribe("Async Testing Examples", () => {
       done();
     }, 1000);
   });
+
+  // check BrowserSynchrounos Events, to have correct return value in the view,  after event (check zone[Zone.js])
+  // zone --> is going to detect all Browser-Sync-Events
+  it("Asynchrounous test example - setTimeout()", fakeAsync(() => {
+    let test = false;
+    //
+    setTimeout(() => {
+      console.log("runnig assertions");
+      test = true;
+      expect(test).toBeTruthy();
+    }, 1000);
+    tick(500);
+    // tick()  steuert die zeitlichen Ablauf innerhalb der fakeAsync()-function
+    tick(499);
+    tick(1);
+  }));
 });
