66ebe5d77d861bd974e22872f4ab35b788880895
Marcus Bergner <marcus.bergner@saxsys.de>
2020-02-28 13:55:18 +0100

    22. testing Angular Smart or container Components - Test Setup

---
+++ angular-testing-course-1-start/src/app/courses/home/home.component.spec.ts
@@ -0,0 +1,56 @@
+import { DebugElement } from "@angular/core";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { CoursesModule } from "../courses.module";
+import { CoursesService } from "../services/courses.service";
+import { HomeComponent } from "./home.component";
+
+// this is an example for testing "smart-components/ container-components" [most Observables]
+// its a top-level-component (fetch the data for this application to needs from a service )
+// testing the conditional logic in the template
+
+describe("HomeComponent", () => {
+  let fixture: ComponentFixture<HomeComponent>;
+  let component: HomeComponent;
+  let testDebugElement: DebugElement;
+
+  beforeEach(async(() => {
+    const coursesServiceSpy = jasmine.createSpyObj("CoursesService", [
+      "findAllCourses"
+    ]);
+    TestBed.configureTestingModule({
+      imports: [
+        CoursesModule,
+        // Noop --> No operation
+        NoopAnimationsModule
+      ],
+      providers: [{ provide: CoursesService, useValue: coursesServiceSpy }]
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        testDebugElement = fixture.debugElement;
+      });
+  }));
+
+  it("should create the component", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("should display only beginner courses", () => {
+    pending();
+  });
+
+  it("should display only advanced courses", () => {
+    pending();
+  });
+
+  it("should display both tabs", () => {
+    pending();
+  });
+
+  it("should display advanced courses when tab clicked", () => {
+    pending();
+  });
+});
