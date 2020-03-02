0b74c8955c815f67e9d88ddb2fdde3b74dca4449
Marcus Bergner <marcus.bergner@saxsys.de>
2020-03-02 11:13:52 +0100

    29/Testing Promised-based code- Introduction to MicroTasks

--- angular-testing-course-1-start/src/app/courses/home/async-examples.spec.ts
+++ angular-testing-course-1-start/src/app/courses/home/async-examples.spec.ts
@@ -1,5 +1,7 @@
 import { fakeAsync, flush } from "@angular/core/testing";
 
+// Browser-Runtime --> has 2 different types of Asynchronous operations
+//
 describe("Async Testing Examples", () => {
   it("Asynchronous test example with Jasmine done()", (done: DoneFn) => {
     let test = false;
@@ -15,13 +17,15 @@ describe("Async Testing Examples", () => {
   // check BrowserSynchrounos Events, to have correct return value in the view,  after event (check zone[Zone.js])
   // zone --> is going to detect all Browser-Sync-Events
   it("Asynchrounous test example - setTimeout()", fakeAsync(() => {
+    // Test-Setup
     let test = false;
-    //
+
     setTimeout(() => {});
     setTimeout(() => {
       console.log("runnig assertions");
       test = true;
     }, 1000);
+    // Test-Body
     // tick(500);
     // // tick()  steuert die zeitlichen Ablauf innerhalb der fakeAsync()-function
     // tick(499);
@@ -29,4 +33,37 @@ describe("Async Testing Examples", () => {
     flush();
     expect(test).toBeTruthy();
   }));
+  fit("Asynchronouns tes examplwe - plain Promise", () => {
+    // test a complex component that uses internally promises, by Micotasks-Queue and Tasks-Queue!
+    // browser is goining to execute any synchronous code here, then is going to empty Micro-Task-Queue !
+    let test = false;
+
+    // create a promise
+    // add to Task-Queue == Mager-TaskQueue
+    console.log("Creating promise !");
+    setTimeout(() => {
+      console.log("setTimeout() first callback triggered.");
+    });
+    console.log("Creating promise !");
+    setTimeout(() => {
+      console.log("setTimeout() second callback triggered.");
+    });
+
+    // create a promise - chain
+    // add to Micro-Task-Queue --> this will execute any micro-queue-tasks(resolve all promises scheduled) before going over to Task-Queue
+    Promise.resolve()
+      .then(() => {
+        console.log("Promise first then() evaluated successfully !");
+        // add new promise to Micro-Task-Queue
+        return Promise.resolve();
+      })
+      // second then()-block -->corresponding to second promise
+      .then(() => {
+        console.log("Promise second then() evaluated successfully !");
+
+        test = true;
+      });
+    console.log("Running test assertions");
+    expect(test).toBeTruthy();
+  });
 });
